3eba8f799060c6b12e941e82870a4c8b04712587
Kit Zihlman <hnz.co@outlook.com>
2024-10-02 12:40:20 -0600

    Wrote the JS

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//Pseudocode:
+//Create a "Hello World" message
+//Add an alert that the page being viewed is under construction
+//Link into the portfolio page
+
+const greeting = "Hello World! ";
+const construction = "The page you are viewing is currently under construction. Please check back later for more updates!";
+alert(greeting + construction);
