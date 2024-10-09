95be16c09a8323fe74505d55df44be5bcdb9ce61
Kit Zihlman <hnz.co@outlook.com>
2024-10-08 23:01:59 -0600

    Fixed javascript.js and wrote success.js

--- javascript.js
+++ javascript.js
@@ -2,9 +2,20 @@
 //Create a "Hello World" message
 //Add an alert that the page being viewed is under construction
 //Link into the portfolio page
+//Copy and paste the form JS into this document from Block12
+//Link into the contact page
 
-const greeting = "Hello World! ";
-const construction = "The page you are viewing is currently under construction. Please check back later for more updates!";
-alert(greeting + construction);
+if (document.body.dataset.showAlert === "true") {
+    alert("Hello World! This page is still under construction. Please check back later for more updates!")
+}
 
 //The above creates a pop up alert that combines the two strings into a message.
+
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
---
+++ success.js
@@ -0,0 +1,6 @@
+//This file controls the popup message that is rendered after the form in 
+//"contact.html is submitted."
+
+setTimeout(() => {
+    window.location.href = window.location.origin
+  }, 5000)
